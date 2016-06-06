efc6f7eb1c120f6b5a948812247093580978102a
macbook <macbook@macbooks-MacBook-Pro.local>
2016-06-06 20:11:37 +0200

    add last update + clean project

---
+++ srcs/ft_exec.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exec.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebersol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/09 17:23:19 by eebersol          #+#    #+#             */
+/*   Updated: 2016/04/09 17:23:20 by eebersol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <21sh.h>
+
+void	ft_(void)
+{
+	t_sh *sh;
+	t_prompt *prompt;
+	char **db_tab;
+	char **cmd;
+	int i;
+
+	sh = ft_sh();
+	i = 0;
+	prompt = sh->prompt;
+	db_tab = ft_strsplit(prompt->complet_prompt, ';');
+	while (i < ft_tab_len(db_tab))
+	{
+		cmd = ft_word_to_tab(db_tab[i]);
+		if (cmd[0])
+			minishell_exec(sh->env, cmd);
+		i++;
+	}
+}
+
+int		ft_exec(char **cmd, char **env)
+{
+	pid_t	pid;
+	int		status;
+	int		flag;
+
+	flag = 0;
+	if ((pid = fork()) == 0)
+	{
+		if ((execve(cmd[0], cmd, env)) != -1)
+			flag = -1;
+		flag = 1;
+	}
+	wait(&status);
+	return (flag);
+}
---
+++ srcs/ft_minishell_exec.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_minishell_exec.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebersol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/09 17:23:19 by eebersol          #+#    #+#             */
+/*   Updated: 2016/04/09 17:23:20 by eebersol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <21sh.h>
+
+void	minishell_exec(t_list *env, char **cmd)
+{
+	t_sh *sh;
+	t_prompt *prompt;
+	char	*varname;
+	char	**tab_env;
+
+	varname = cmd[0];
+	sh = ft_sh();
+	prompt = sh->prompt;
+	tab_env = ft_list_to_tab(&env);
+	if ((ft_strcmp(cmd[0], "cd")) == 0)
+		ft_cd(env, cmd);
+	else if ((ft_strcmp(cmd[0], "env")) == 0)
+		ft_env_opt(env, cmd);
+	else if ((ft_strcmp(cmd[0], "setenv")) == 0)
+	{
+		if ((ft_error_set_unset_env(cmd, "setenv")) == 0)
+			ft_env_set(&env, cmd[1], cmd[2]);
+	}
+	else if ((ft_strcmp(cmd[0], "unsetenv")) == 0)
+		*env = *ft_env_unset(&env, cmd);
+	else if ((ft_strcmp(cmd[0], "exit")) == 0)
+		ft_exit(cmd, &env);
+	else if ((cmd[0][0] == '.' && cmd[0][1] == '/') || cmd[0][0] == '/')
+		ft_exec(cmd, tab_env);
+	//else if (((cmd[0] = ft_get_path(env, cmd[0])) != NULL))
+	//{
+	//	ft_exec(cmd, tab_env);
+	//}
+	else if (1)
+	{
+		ft_main_parser();
+	}
+}
